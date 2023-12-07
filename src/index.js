40e8ef66040fd0776ff7e5db874743ca39a41793
Lars Hvam <larshp@hotmail.com>
2023-12-07 11:47:11 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,48 @@
 const core = require('@actions/core');
 const child_process = require('child_process');
 const fs = require('fs');
-const rename = require('./rename.js');
+
+const inputFolder = "temp-abap2xlsx-input";
+const outputFolder = "temp-abap2xlsx-output";
+
+const config = {
+  "global": {
+    "files": "/" + inputFolder + "/src/*.*",
+    "skipGeneratedGatewayClasses": true,
+    "skipGeneratedPersistentClasses": true,
+    "skipGeneratedFunctionGroups": true,
+    "useApackDependencies": false
+  },
+  "dependencies": [
+    {
+      "url": "https://github.com/abaplint/deps",
+      "folder": "/deps",
+      "files": "/src/**/*.*"
+    }
+  ],
+  "rename": {
+    "output": outputFolder,
+    "patterns": []
+  },
+  "syntax": {
+    "version": "v702",
+    "errorNamespace": "^(Z|Y)"
+  },
+  "rules": {
+    "begin_end_names": true,
+    "check_ddic": true,
+    "check_include": true,
+    "check_syntax": true,
+    "global_class": true,
+    "implement_methods": true,
+    "method_implemented_twice": true,
+    "parser_error": true,
+    "parser_missing_space": true,
+    "superclass_final": false,
+    "unknown_types": true,
+    "xml_consistency": true
+  }
+}
 
 try {
   const patterns = core.getInput('patterns');
@@ -10,13 +51,10 @@ try {
   const folder = core.getInput('folder');
   console.log(`Folder: ${folder}`);
 
-  const inputFolder = "temp-abap2xlsx-input";
-  const outputFolder = "temp-abap2xlsx-output";
-
   child_process.execSync(`npm install @abaplint/cli -g`);
   child_process.execSync(`git clone --depth 1 https://github.com/abap2xlsx/abap2xlsx ` + inputFolder);
   child_process.execSync(`mkdir ` + outputFolder);
-  fs.writeFileSync("abaplint-rename.json", rename.config);
+  fs.writeFileSync("abaplint-rename.json", JSON.stringify(config, null, 2));
 //  child_process.execSync(`abaplint --rename abaplint-rename.json`);
   child_process.execSync(`ls`);
 
